2cc58b99857ddc14d42fafaf10c3e3dfbf04e072
nayan-bansal <nayan.7991@gmail.com>
2020-10-09 05:00:05 +0530

    Added: Mood Analyzer to check Any and Sad mood

--- MoodAnalyzerTest.java
+++ MoodAnalyzerTest.java
@@ -7,12 +7,21 @@ import org.junit.Test;
 public class MoodAnalyzerTest {
 
 @Test
-public void Analyze() {
+public void Analyze_Happy() {
 
 	MoodAnalyzer moodAnalyzer = new  MoodAnalyzer();
 
 	String mood = moodAnalyzer.moodAnalyze_Sad("I am in Any Mood");
 	 assertSame("Please check the method in main", "HAPPY", mood);
+	}
+
+@Test
+public void Analyze_Sad() {
+
+	MoodAnalyzer moodAnalyzer = new  MoodAnalyzer();
+
+	String mood = moodAnalyzer.moodAnalyze_Sad("I am in Sad Mood");
+	 assertSame("Please check the method in main", "SAD", mood);
 	}
 }
 

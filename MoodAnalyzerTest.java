cec74967f47163ec71ec11358ddf80bb6cabfd7a
nayan-bansal <nayan.7991@gmail.com>
2020-10-09 06:17:05 +0530

    Added: Given null should return happy with try exception

--- MoodAnalyzerTest.java
+++ MoodAnalyzerTest.java
@@ -6,13 +6,21 @@ import org.junit.Test;
 
 public class MoodAnalyzerTest {
 
-
 @Test
-public void Analyze_Happy_Shoul_return_Happy_Mood() {
+public void Analyze_Happy() {
 
 	MoodAnalyzer moodAnalyzer = new  MoodAnalyzer();
 
-	String mood = moodAnalyzer.moodAnalyze_Sad("I am in Happy Mood");
+	String mood = moodAnalyzer.moodAnalyze_Sad("I am in Any Mood");
+	 assertSame("Please check the method in main", "HAPPY", mood);
+	}
+
+@Test
+public void Analyze_Sad() {
+
+	MoodAnalyzer moodAnalyzer = new  MoodAnalyzer();
+
+	String mood = moodAnalyzer.moodAnalyze_Sad(null);
 	 assertSame("Please check the method in main", "HAPPY", mood);
 	}
 }

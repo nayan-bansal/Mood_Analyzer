6c4e4a09c75ae515182dcec6fa516f1d59896096
nayan-bansal <nayan.7991@gmail.com>
2020-10-09 06:39:14 +0530

    Added: Given null method, throws exception

--- MoodAnalyzerTest.java
+++ MoodAnalyzerTest.java
@@ -11,18 +11,14 @@ public void Analyze_Happy() {
 
 	MoodAnalyzer moodAnalyzer = new  MoodAnalyzer();
 
-	String mood = moodAnalyzer.moodAnalyze_Sad("I am in Any Mood");
-	 assertSame("Please check the method in main", "HAPPY", mood);
+	try {
+		String mood = moodAnalyzer.moodAnalyze();
+		assertSame("Please provide valid details, dont provide null", mood);
+	} 
+	catch (MoodAnalysisException e) {
+		e.getMessage();
 	}
 
-@Test
-public void Analyze_Sad() {
-
-	MoodAnalyzer moodAnalyzer = new  MoodAnalyzer();
-
-	String mood = moodAnalyzer.moodAnalyze_Sad(null);
-	 assertSame("Please check the method in main", "HAPPY", mood);
-	}
 }
-
+}
 
